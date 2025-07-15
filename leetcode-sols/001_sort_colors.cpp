df40e2785c9f377e53704fd4512146e9d22b055f
Wamika <wamikas17@gmail.com>
2025-07-15 13:50:32 +0530

    updated code

--- leetcode-sols/001_sort_colors.cpp
+++ leetcode-sols/001_sort_colors.cpp
@@ -1,22 +1,29 @@
+
+
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
+    void sortColors(vector<int>& nums) 
+    {
         
-        int n = nums.size();
-        bool swapped;
-
-        for(int i =0; i<n-1; i++){
-            swapped = false;
-            for(int j=0; j<n-i-1; j++){
-                if(nums[j] > nums[j+1])
-                {
-                    swap(nums[j], nums[j+1]);
-                    swapped = true;
-                }
+        int low = 0, mid = 0, high = nums.size() -1;
+        while(mid <= high)
+        {
+            if(nums[mid] == 0)
+            {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
             }
-            if (!swapped)
-            break; 
+            else if(nums[mid] == 1)
+            {
+                
+                mid++;
+            }
+            else{
+                 swap(nums[mid], nums[high]);
+                high--;
+            }
+               
         }
     }
 };
-
